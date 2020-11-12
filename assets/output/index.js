c08ff7f06b40bcd32cb3fc7785c0ad640a6d86f8
Rae <rae.rudie@gmail.com>
2020-11-12 11:29:03 -0500

    build out site globals and homepage skeleton

--- assets/output/index.js
+++ assets/output/index.js
@@ -233,7 +233,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58534" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55119" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
