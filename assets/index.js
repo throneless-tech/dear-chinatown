6133f4967b072f6dbcc3d2ba1e7c1a3694348bb2
Rae <rae.rudie@gmail.com>
2020-11-13 15:29:28 -0500

    build and refactor airtable data integration into mapbox map

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,8 @@
 import "./css/styles.css";
 // import "typeface-roboto";
 
-import "./js/nojs.js";
-import "./js/airtable.js";
-import "./js/map.js";
-
 import dotenv from 'dotenv';
 dotenv.config();
+
+import "./js/nojs.js";
+import "./js/map.js";
