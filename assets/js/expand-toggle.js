eb7c2e76c21aaa330e3db75d4c7de818e96d10e7
rudietuesdays <rae@throneless.tech>
2021-01-11 10:29:04 -0500

    fix broken links on homepage to map sections

--- assets/js/expand-toggle.js
+++ assets/js/expand-toggle.js
@@ -1,8 +1,18 @@
 import ExpandToggle from "@threespot/expand-toggle";
 
 const toggles = document.querySelectorAll("[data-expands]");
+const map = document.getElementById('map');
 
 toggles.forEach((element, index) => {
   const el = new ExpandToggle(element);
-  index === 0 ? el.expand() : null;
+
+  if (!!map) {
+    if (window.location.hash) {
+      if (element.dataset.expands === window.location.hash.substr(1)) {
+        el.expand();
+      }
+    }
+  } else {
+    index === 0 ? el.expand() : null;
+  }
 });
--- assets/output/index.js
+++ assets/output/index.js
@@ -33480,9 +33480,19 @@ var _expandToggle = _interopRequireDefault(require("@threespot/expand-toggle"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const toggles = document.querySelectorAll("[data-expands]");
+const map = document.getElementById('map');
 toggles.forEach((element, index) => {
   const el = new _expandToggle.default(element);
-  index === 0 ? el.expand() : null;
+
+  if (!!map) {
+    if (window.location.hash) {
+      if (element.dataset.expands === window.location.hash.substr(1)) {
+        el.expand();
+      }
+    }
+  } else {
+    index === 0 ? el.expand() : null;
+  }
 });
 },{"@threespot/expand-toggle":"../node_modules/@threespot/expand-toggle/dist/expand-toggle.m.js"}],"index.js":[function(require,module,exports) {
 "use strict";
@@ -33522,7 +33532,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55922" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57225" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
