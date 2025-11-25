e29fcf4664a42e3030247d2d8931a1c799b2d34e
Rae <rae@throneless.tech>
2025-11-25 12:41:57 -0500

    Update index.js

--- assets/output/index.js
+++ assets/output/index.js
@@ -30450,6 +30450,22 @@ if (!!map) {
         break;
       }
     }
+    map.addLayer({
+      'id': '3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 10,
+      'paint': {
+        'fill-extrusion-color': '#E0DFC8',
+        // use an 'interpolate' expression to add a smooth transition effect to the
+        // buildings as the user zooms in
+        'fill-extrusion-height': ['interpolate', ['linear'], ['zoom'], 15, 0, 15.05, ['get', 'height']],
+        'fill-extrusion-base': ['interpolate', ['linear'], ['zoom'], 15, 0, 15.05, ['get', 'min_height']],
+        'fill-extrusion-opacity': 0.6
+      }
+    }, labelLayerId);
     map.on('click', 'places', e => {
       const coordinates = e.features[0].geometry.coordinates.slice();
       const name = e.features[0].properties.Name;
